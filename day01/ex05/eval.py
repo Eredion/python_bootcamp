c3247db55e60d55773adf7cd6ad450d20b4aca33
Eredion <alvaro.segovia@hotmail.com>
2020-11-19 13:33:20 +0100

    Finish ex05

--- day01/ex05/eval.py
+++ day01/ex05/eval.py
@@ -1,23 +1,26 @@
 #! /usr/bin/env python3
-
-def check_input(words, coefs):
-       try:
-           if isinstance(words, list) and isinstance(coefs, list) and len(words) == len(coefs):
-               for word in words:
-                   if not isinstance(word, str):
-                       return -1
-               for coeg in coefs:
-                   if not isinstance(coeg, float):
-                       return -1
-               return 1
-           else:
-               return -1
-       except:
-           return -1
+#! /usr/bin/env python3
 
 class Evaluator:
+    @staticmethod
+    def check_input(words, coefs):
+           try:
+               if isinstance(words, list) and isinstance(coefs, list) and len(words) == len(coefs):
+                   for word in words:
+                       if not isinstance(word, str):
+                           return -1
+                   for coeg in coefs:
+                       if not isinstance(coeg, float):
+                           return -1
+                   return 1
+               else:
+                   return -1
+           except:
+               return -1
+
+    @staticmethod
     def zip_evaluate(words, coefs):
-        if check_input(words, coefs) == -1:
+        if Evaluator.check_input(words, coefs) == -1:
             return -1
         ret = 0
         zipper = zip(words, coefs)
@@ -25,8 +28,9 @@ class Evaluator:
             ret += len(elem[0]) * elem[1]
         return ret
     
+    @staticmethod
     def enumerate_evaluate(words, coefs):
-        if check_input(words, coefs) == -1:
+        if Evaluator.check_input(words, coefs) == -1:
             return -1
         ret = 0
         for i, j in enumerate(words):
